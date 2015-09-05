d2d69180e12897584b590af9e06400c46b62b780
tjstyles1 <tyler_n92@hotmail.com>
2015-09-05 18:01:42 +1000

    Initial commit

---
+++ app/src/main/java/com/tjstyles/test1969/menu1_Fragment.java
@@ -0,0 +1,21 @@
+package com.tjstyles.test1969;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tyler on 21/08/2015.
+ */
+public class menu1_Fragment extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.manu1_layout, container,false);
+        return rootview;
+    }
+}
